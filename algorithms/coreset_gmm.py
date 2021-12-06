36b49530bb15f1ae5875c2a25aa54f1ebaea62f4
Andrew Roberts <agr.roberts@gmail.com>
2021-12-06 11:05:27 -0500

    Major code organization changes; moved various functions out of classes and into helper functions file

--- algorithms/coreset_gmm.py
+++ algorithms/coreset_gmm.py
@@ -1,106 +1,17 @@
 import matplotlib.pyplot as plt
+import helper_functions as hf
 import numpy as np
 import warnings
 
 class Coreset_GMM:
 	def __init__(self, rng, x_arr, k, epsilon, spectrum_bound, delta): 
 		self.rng = rng
-		self.x_array = x_arr
+		self.x_arr = x_arr
 		self.k = k
 		self.epsilon = epsilon
 		self.spectrum_bound = spectrum_bound
 		self.delta = delta
-		self.d = self.x_array.shape[1]
-
-	# Return l2 distance between 2 1D arrays.	
-	def dist(self, x, y): 
-		return(np.linalg.norm(x - y))
-
-	# Return l2 distance between a point (array) and set (of arrays)
-	# Note: If Y is a 2D ndarray, the rows of Y will be considered the "points". 
-	def dist_set(self, x, Y):
-		return min([self.dist(x, y) for y in Y])
-
-	# Plot points if x_array has dimension 1 or 2
-	# arr = None means default to plotting self.x_array.
-	# ax = None means create new axis, otherwise adds to existing one
-	def scatter_2D(self, arr = None, ax = None):
-		if self.d > 2: 
-			warnings.warn("Cannot plot data of dimension > 2")
-		else: 	
-			if arr is None:
-				arr = self.x_array
-
-			n = len(arr)
-			x = [arr[i][0] for i in range(n)]
-			
-			if self.d == 1:
-				y = np.zeros(shape = n)
-			else:
-				y = [arr[i][1] for i in range(n)]
-	
-			if ax is None:
-				f, ax = plt.subplots()	
-			ax.scatter(x, y)
-
-			return ax
-		
-	# Computes sum of squared distances between a set of "centers" B and a set of points X. 
-	# If X is None, then X := self.x_array
-	def cluster_cost(self, B, X = None):
-		if X is None:
-			X = self.x_array
-
-		return np.sum([self.dist_set(x, B)**2 for x in X])
-	
-		
-	# Implementation of k-Means++ algorithm, which initializes the k means to be used
-	# in naive k-Means. 
-	def kmeans_pp(self):
-		B = np.zeros(shape = (self.k, self.d))
-		B[0] = self.rng.choice(self.x_array)
-
-		for j in range(1, self.k):
-			dists_to_B = [self.dist_set(x, B[:j])**2 for x in self.x_array]
-			probs = dists_to_B / np.sum(dists_to_B)
-			B[j] = self.rng.choice(self.x_array, p = probs)
-			
-		return (B, self.cluster_cost(B))
-
-	# Standard K-Means Algorithm with option to use non-uniform point weights
-	# If w is None, uses uniform weights (i.e. standard KMeans)
-	# If centers_init is None, randomly initializes the k center according to point weights. 
-	# If centers_init is 'kmeans++' initializes ceners using kmeans++ algorithm. 
-	def weighted_kmeans(self, w = None, centers_init = None, tol = 1e-4):
-		# TODO: option to scale input
-		n = len(self.x_array)
-
-		# Determine points weights
-		if w is None:
-			w = np.repeat([1 / n], [n])
-		else: 
-			w = np.array(w) / np.sum(w)
-
-		# Initialize k centers
-		if centers_init is None:
-			centers = self.rng.choice(self.x_array, size = self.k, p = w)
-		elif centers_init == 'kmeans++':
-			centers = self.kmeans_pp()[0]
-	
-		while True:
-			# Assign points to clusters
-			point_assignments = np.array([int(np.argmin([self.dist(x, c) for c in centers])) for x in self.x_array], dtype = np.int16)
-
-			# Compute new centers
-			centers_prev = np.array(centers)
-			centers = np.array([np.average(self.x_array[point_assignments == j], axis = 0, weights = w[point_assignments == j]) for j in range(self.k)])
-
-			# Stop when relative change in clusters is small (in Frobenius norm)				
-			if np.linalg.norm(centers - centers_prev) / np.linalg.norm(centers_prev) < tol:
-				print("weighted_kmeans() converged")
-				break
-
-		return centers
+		self.d = self.x_arr.shape[1]
 
 	# Runs kmeans++ specified number of times, then returns the best set of initial values for the k means
 	# in the sense that minimizes squared pairwise distances. 
@@ -111,7 +22,7 @@ class Coreset_GMM:
 		else:
 			warnings.warn("Using manually set number of runs for kmeans initialization algorithm; theoretical bounds not guaranteed.")
 
-		k_means_init = [self.kmeans_pp() for i in range(N)]
+		k_means_init = [hf.kmeans_pp(self.x_arr, self.k, self.rng) for i in range(N)]
 		argmin = np.argmin([b[1] for b in k_means_init])
 
 		return k_means_init[argmin]
@@ -127,7 +38,7 @@ class Coreset_GMM:
 		if B is None: 
 			B, B_cost = self.get_bicriteria_kmeans_approx(N = N_bicriteria_runs)
 		elif B_cost is None:
-			B_cost = np.sum([self.dist_set(x, B)**2 for x in self.x_array])
+			B_cost = np.sum([hf.dist_set(x, B)**2 for x in self.x_arr])
 
 		# TODO: which constant to use? 
 		if m is None: 
@@ -136,7 +47,7 @@ class Coreset_GMM:
 			warnings.warn("Using manually set coreset size; theoretical bounds not guaranteed.")
 			
 		alpha = 16 * (np.log2(self.k) + 2)
-		n = len(self.x_array)
+		n = len(self.x_arr)
 		B_len = len(B)
 
 		print("alpha = ", alpha)
@@ -147,22 +58,22 @@ class Coreset_GMM:
 		closest_centers = np.zeros(shape = n, dtype = np.int16)
 
 		for i in range(n):
-			dists_to_centers = [self.dist(self.x_array[i], b)**2 for b in B]
+			dists_to_centers = [hf.dist(self.x_arr[i], b)**2 for b in B]
 			closest_centers[i] = int(np.argmin(dists_to_centers))
 			point_costs[i] = dists_to_centers[closest_centers[i]]
 
-		closest_centers = np.array([np.argmin([self.dist(x, b) for b in B]) for x in self.x_array])
+		closest_centers = np.array([np.argmin([hf.dist(x, b) for b in B]) for x in self.x_arr])
 		B_cluster_sizes = [int(np.sum(closest_centers == j)) for j in range(B_len)]
-		B_cluster_costs = [self.cluster_cost(B, self.x_array[self.x_array == j]) for j in range(B_len)]
+		B_cluster_costs = [hf.cluster_cost(self.x_arr[self.x_arr == j], B) for j in range(B_len)]
 
 		# Weight points and sample based on weighting to create coreset	
-		sampling_weights = np.array([self.calc_point_weight(alpha, self.x_array[i], point_costs[i], 
+		sampling_weights = np.array([self.calc_point_weight(alpha, self.x_arr[i], point_costs[i], 
 									                   	    B_cluster_sizes[closest_centers[i]], 
 													   		B_cluster_costs[closest_centers[i]], B_cost) for i in range(n)])	
 		sampling_weights = sampling_weights / np.sum(sampling_weights)
 
 		# Calculate weights for each point in coreset	
-		coreset = self.rng.choice(self.x_array, size = m, p = sampling_weights)
+		coreset = self.rng.choice(self.x_arr, size = m, p = sampling_weights)
 		coreset_weights = np.array([1 / x for x in sampling_weights]) / m	
 			
 		return(coreset, coreset_weights)
