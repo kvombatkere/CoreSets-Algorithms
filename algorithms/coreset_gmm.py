Fixed bug in calculation of GMM coreset weights

algorithms/coreset_gmm.py
@@ -73,10 +73,10 @@ class Coreset_GMM:
 		sampling_weights = sampling_weights / np.sum(sampling_weights)
 
 		# Calculate weights for each point in coreset	
-		coreset = self.rng.choice(self.x_arr, size = m, p = sampling_weights)
-		coreset_weights = np.array([1 / x for x in sampling_weights]) / m	
-			
-		return(coreset, coreset_weights)
+		coreset_selector = self.rng.choice(np.arange(self.x_arr.shape[0]), size = m, p = sampling_weights)
+		coreset_weights = np.array([1 / sampling_weights[i] for i in coreset_selector])
+	
+		return(self.x_arr[coreset_selector], coreset_weights)
 		
 		
 		

algorithms/helper_functions.py
@@ -56,6 +56,24 @@ def cluster_cost(x_arr, B):
 # k-means Functions
 # ------------------------------------------------------------------------
 
+def get_point_assignments(x_arr, centers):
+	# Given data set and a set of "centers", return a vector that assigns each point to its 
+	# closest center (in Euclidean/l2 distance).
+	#
+	# Args:
+	#	x_arr: numpy array of shape (# observations, # features). 
+	#   centers: numpy array of shape (# centers, # features). 
+	#
+	# Returns:
+	#	numpy array of shape '# observations'. The ith entry of this array is an integer 
+	#	in {0, ..., # centers - 1}, indicating the index of 'centers' containing the center 
+	#	closest to point i in Euclidean distance. 	
+
+	return np.array([int(np.argmin([dist(x, c) for c in centers])) for x in x_arr], dtype = np.int16)
+
+
+
+
 def kmeans_pp(x_arr, k, rng):
 	# Implementation of k-Means++ algorithm, which initializes the k means to be used
 	# in naive k-Means. 
@@ -114,7 +132,7 @@ def weighted_kmeans(x_arr, k, rng, w = None, centers_init = None, tol = 1e-4):
 
 	while True:
 		# Assign points to clusters
-		point_assignments = np.array([int(np.argmin([dist(x, c) for c in centers])) for x in x_arr], dtype = np.int16)
+		point_assignments = get_point_assignments(x_arr, centers)
 
 		# Compute new centers
 		centers_prev = np.array(centers)

algorithms/weighted_gmm.py
@@ -3,8 +3,8 @@
 #
 # Andrew Roberts
 
-
 import matplotlib.pyplot as plt
+import helper_functions as hf
 import numpy as np
 import warnings
 
@@ -15,23 +15,40 @@ class Weighted_GMM:
 		self.k = k
 		self.prior_threshold = prior_threshold
 
-	def fit(self, x_array, w_points):
+	def fit(self, x_arr, w_points = None):
 
-		# Initialize matrix of cluster responsibilities; r_ij = p(point i in cluster j|x_i, mean_j, cov_j)
-		# TODO: need to turn this into matrix
-		r = self.weighted_kmeans(w_points, centers_init = 'kmeans++')
-		
+		# If not provided, uniform weights are used.
+		if w_points is None:
+			w_points = np.ones(shape = x_arr.shape[0])
+		w_points = np.array(w_points) / np.sum(w_points)
+
+		n = x_arr.shape[0]
+
+		# Initialize matrix of cluster responsibilities; R_ij = p(point i in cluster j|x_i, mean_j, cov_j)
+		# Initial values come from kmeans "hard clustering"; all probability mass assigned to one cluster for each point.
+		means_init = hf.weighted_kmeans(x_arr, self.k, self.rng, w_points, centers_init = 'kmeans++')
+		r = hf.get_point_assignments(x_arr, means_init)		
+		R = np.zeros(shape = (n, self.k))
+		R[np.arange(n), r] = 1
+
+		print(R[:20, :])
+
+
+		'''
+
 		# Optimize parameters given initial cluster responsibilities
-		w_clusters, means, covs = self.maximization(x_array, r)
+		w_clusters, means, covs = self.maximization(x_arr, R)
 
 		while True: 
 			# E Step: Compute responsibilities given parameters
-			r = self.expectation(X, w_points, w_clusters, means, covs)
+			R = self.expectation(x_arr, w_points, w_clusters, means, covs)
 		
 			# Store old parameters for comparison
 	
 			# M Step: Optimize parameters given responsibilities
-			w_clusters, means, covs = self.maximization(x_array, r)
+			w_clusters, means, covs = self.maximization(x_arr, R)
+
+		'''
 
 	def auxiliary_function(self, R, w_points, w_clusters, P):
 		# Computes the standard EM for GMM auxiliary function Q(theta, theta^(t - 1)), modified so each term 

test/gmm_test.py
@@ -31,6 +31,7 @@ means1 = [[5, 5], [-5, -5], [0, 0]]
 covs1 = [np.array([[1, 0], [0, 1]]), np.array([[1, 0], [0, 1]]), np.array([[7, 0], [0, 1]])]
 arr1 = hf.simulate_gaussian_clusters(rng, [3000, 2000, 5000], 3, means1, covs1)
 
+'''
 
 #
 # Test GMM data simulation
@@ -113,7 +114,6 @@ ax = hf.scatter_2D([arr1, C], title = 'Coreset of size 100')
 plt.show()
 
 
-
 #
 # Test Weighted KMeans
 #
@@ -135,6 +135,30 @@ w = np.concatenate([w1, w2, w3])
 centers = hf.weighted_kmeans(arr1, k, rng, w = w, centers_init = 'kmeans++')
 ax = hf.scatter_2D([arr1, centers], title = 'Weighted kmeans: Weights Proportional to Within-Cluster Variance Contribution')
 plt.show()
+
+'''
+
+#
+# Test Weighted GMM 
+#
+
+# Setup
+k = 3
+prior_threshold = .001
+spectrum_bound = .001
+delta = .01
+eps = .01
+
+# Instantiate GMM Coreset and Weighted GMM objects
+coreset = gmm.Coreset_GMM(rng, arr1, k, eps, spectrum_bound, delta)
+gmm_model = wgmm.Weighted_GMM(rng, k, prior_threshold)
+
+# Compute coreset of size 1% of data
+C, w = coreset.generate_coreset(m = int(np.floor(.01 * len(arr1))))
+
+# Fit GMM model on coreset
+gmm_model.fit(C, w)
+
 
 
 