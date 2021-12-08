59f5d0b55553b44393f3c9a65ce1aa810a83473a
Bhushan Suwal <bhushansuwal@Bhushans-MacBook-Pro.local>
2021-12-07 19:11:46 -0500

    changed representation of point to be a tuple (point, weight)

--- algorithms/coreset_streaming.py
+++ algorithms/coreset_streaming.py
@@ -4,13 +4,13 @@ Class implementing something something TODO
 Bhushan Suwal
 Dec 2021
 
-Procedure:
-keep adding points
-snap()
-build_coreset()
-
 TODO:
-implement coresets
+implement how set union would work
+test union
+run it on a stream
+figure initial resolution
+numpy-ify everything
+clean everything
 """
 
 import numpy as np
@@ -21,15 +21,14 @@ class Coreset_Streaming:
     def __init__(self):
         self.resolution = 1
         self.side_length = 2
-        # keys are points, values are weights
-        self.coreset = dict()
-        self.point_bucket = [] # stores points before building point_bucket
+        self.coreset = [] # corresponding index has weight in weights
         self.grid_points = dict() # keys are grid_points, values are arrays of points
 
-    def add_point_to_bucket(self, point):
+    def add_point(self, point):
         """
         """
-        self.point_bucket.append(point)
+        weight = 1
+        self.coreset.append((point, weight))
 
     def grid_location(self, point):
         """ We identify grid locations by the smallest value of each dimension
@@ -37,31 +36,41 @@ class Coreset_Streaming:
         """
         loc = []
         for dim in point:
-            loc.append(dim - dim % side_length)
-        return np.array(loc)
+            loc.append(dim - dim % self.side_length)
+        return loc
 
-    def snap(self):
+    def snap_points_to_grid(self):
         """
         """
-        for point in self.point_bucket:
-            loc = self.grid_location(point)
+        self.grid_points = dict()
+        for (point, weight) in self.coreset:
+            # hack: lists can't be dict keys, so a hack is to turn them into strings
+            loc = repr(self.grid_location(point))
             if loc in self.grid_points.keys():
-                self.grid_points[loc].append(point)
+                self.grid_points[loc].append((point, weight))
             else:
-                self.grid_points[loc] = [point]
+                self.grid_points[loc] = [(point, weight)]
 
-    def build_coreset(self):
+    def build_coreset_from_grid(self):
         """ Assumes points have already been "snapped".
         """
-        for (loc, points) in self.grid_points:
-            representative = random.choice(self.grid_points[loc])
-            weight = len(self.grid_points[loc])
-            self.coreset[representative] = weight
-        self.point_bucket = []
+        self.coreset = []
+        for (loc, points) in self.grid_points.items():
+            (representative, _) = random.choice(self.grid_points[loc])
+            repr_weight = 0
+            for (point, weight) in self.grid_points[loc]:
+                repr_weight += weight
+            self.coreset.append((representative, repr_weight))
+
+    def build_coreset(self):
+        """
+        """
+        self.snap_points_to_grid()
+        self.build_coreset_from_grid()
 
     def double_resolution(self):
         """
         """
-        resolution *= 2
-        side_length *= 2
-        self.snap()
+        self.resolution += 1
+        self.side_length *= 2
+        self.build_coreset()
