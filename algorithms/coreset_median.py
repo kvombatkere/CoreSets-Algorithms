519a41310f2af25b9e8d2b63281ce8b9e37bbb26
Karan Vombatkere <kvombatkere@gmail.com>
2021-12-08 22:21:24 -0500

    median estimation testing

--- algorithms/coreset_median.py
+++ algorithms/coreset_median.py
@@ -15,42 +15,48 @@ class Coreset_Median:
 
     #Initialize with data and epsilon value
     def __init__(self, x_arr, epsilon):
-        self.x_array = sorted([x[0] for x in x_arr])
-        print('Length of input = ', len(self.x_array))
+        self.x_array = sorted(x_arr)
+        #print('Length of input = ', len(self.x_array))
         self.epsilon = epsilon
+
+        self.numPartitions = 0
         
 
     #Partition and create coresets    
     def partitionSubsequences(self):
-        numPartitions = int(1/self.epsilon) + 1
-        partitionSize = int(len(self.x_array)/numPartitions)
+        self.numPartitions = int(1/self.epsilon) + 1
+        partitionSize = int(len(self.x_array)/self.numPartitions)
+        print(partitionSize)
 
         coreset_Arr = []
         for i in range(0, len(self.x_array), partitionSize):
             coreset_Arr.append(self.x_array[i])
 
-        print("Total elements checked = {}".format(numPartitions))
+        print("Total elements checked = {}".format(self.numPartitions))
 
         return coreset_Arr
 
 
     #Compute median value
     def compute_median(self):
+        #Compute approx median
         startTime = time.perf_counter()
 
         coreset_array = self.partitionSubsequences()
-        medianVal = coreset_array[int(len(coreset_array)/2)]
+        self.approxMedianVal = coreset_array[int(len(coreset_array)/2)]
 
-        print("Median value approximation = {}".format(medianVal))
+        print("Median value approximation = {}".format(self.approxMedianVal))
 
-        runTime = time.perf_counter() - startTime
-        print("Median approximation computation time = {} seconds".format(runTime))
+        self.runTime = time.perf_counter() - startTime
+        print("Median approximation computation time = {:.5f} seconds".format(self.runTime))
 
-        startTime = time.perf_counter()
-        print("True Median = {}".format(np.median(self.x_array)))
-        runTime = time.perf_counter() - startTime
-        print("Numpy Median computation time = {} seconds".format(runTime))
+        #Compute true median
+        np_startTime = time.perf_counter()
+
+        self.trueMedian = np.median(self.x_array)
+        print("True Median = {}".format(self.trueMedian))
+
+        self.np_runTime = time.perf_counter() - np_startTime
+        print("Numpy Median computation time = {:.5f} seconds".format(self.np_runTime))
         
         return 
-
-
